65a23a7423d6b17a4bbbed6fafa01c0731539d0f
completelyAbsorbed <completelyAbsorbed@users.noreply.github.com>
2022-02-06 20:35:47 -0700

    ignore learning file

--- learning.R
+++ learning.R
@@ -6,3 +6,7 @@
 library(pdftools)
 library(tesseract) # for OCR with pdftools library
 
+dir <- "C:/Users/liz/iCloudDrive/dhamma/canon/"
+suttas_filename <- "SuttaPitaka211009.pdf"
+
+suttas <- pdf_text("")

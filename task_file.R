0843b730bcfd6a15876e60d6b6b051990f203ba0
completelyAbsorbed <completelyAbsorbed@users.noreply.github.com>
2022-02-08 10:39:58 -0700

    Create task_file.R
    tracking tasks for Random Forest Tradition

---
+++ task_file.R
@@ -0,0 +1,41 @@
+################################################################################
+# 
+# task file!
+# 
+# ... this file is for tracking tasks I want to complete, and their level of
+# ... ... completion. may graduate to something more sophisticated eventually,
+# ... ... or maybe not!
+# 
+################################################################################
+
+################################################################################
+# 
+# tasks I want to work on
+# 
+################################################################################
+
+# create a function that notes location of each new passage
+
+# create a function that separates passages into $passage and $notes
+
+# deriving a table of contents
+
+# mapping out references between passages based on Ajaan Geoff's notes
+
+################################################################################
+# 
+# tasks I am working on
+# 
+################################################################################
+
+################################################################################
+# 
+# tasks that are completed
+# 
+################################################################################
+
+################################################################################
+# 
+# tasks that have been set aside for now
+# 
+################################################################################

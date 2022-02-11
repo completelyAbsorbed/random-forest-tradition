46be8cfc2fb67f24c57ef8122cdc42ed070cf593
completelyAbsorbed <completelyAbsorbed@users.noreply.github.com>
2022-02-10 20:09:10 -0700

    Update learning.R

--- learning.R
+++ learning.R
@@ -6,7 +6,23 @@
 library(pdftools)
 library(tesseract) # for OCR with pdftools library
 
-dir <- "C:/Users/liz/iCloudDrive/dhamma/canon/"
+dir <- "C:/Users/liz/iCloudDrive/dhamma/canon/"  # suppress this for github #############################################################################
 suttas_filename <- "SuttaPitaka211009.pdf"
 
-suttas <- pdf_text("")
+suttas <- pdf_text(paste(dir,
+                         suttas_filename,
+                         sep = ""))
+
+length(suttas) # 4044
+
+# suttas[1111]
+
+cat(suttas[1112])
+
+cat(suttas[1113]) # new verse header (MN 82)
+
+cat(suttas[1107]) # new verse header (MN 78)
+
+suttas[1107] # looking for format cues in note reference (link not shown, just number) numeric representation may be the cue 
+
+cat(suttas[1107:1113])
